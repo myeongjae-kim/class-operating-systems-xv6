69796bf5dad3fd32d85f3bb4080deb28061ab17f
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2017-03-26 01:21:06 +0900

    comments are edited.

--- proj_shell/shell.c
+++ proj_shell/shell.c
@@ -31,23 +31,16 @@ static const int STR_BUFF_SIZE = 512;
 static const char* STDIN_FILEPATH = "/dev/fd/0";
 
 void readInstruction(const char* filePath);
-void parseAndExecute(char* inputString, int* numberOfChildProcesses);
+void parseAndExecute(char* instructionBuffer, int* numberOfChildProcesses);
 
 int main(int argc, char *argv[])
 {
     /** If argc is zero, interactive mode, else batch mode. */
     if (argc == 1) {
-        if (DEBUGGING) {
-            printf("Mode: Interactive\n");
-        }
-
+        /** interactive mode */
         readInstruction(STDIN_FILEPATH); 
-
     } else {
-        if (DEBUGGING) {
-            printf("Mode: Batch\n");
-        }
-
+        /** batch mode for all command line arguments*/
         for (int i = 1; i < argc; ++i) {
             readInstruction(argv[i]);
         }
@@ -60,44 +53,48 @@ void readInstruction(const char* filePath){
     /** Read file to the end. */
     FILE* fp = fopen(filePath, "r");
     if (fp == NULL) {
-        // file open failed. do nothing.
+        /** file open failed. do nothing. */
         return;
     }
     
+    /** if file is stdin, turn on interactiveMode. */
     bool interactiveMode = false;
     if(strncmp(STDIN_FILEPATH, filePath, sizeof(STDIN_FILEPATH) / sizeof(char)) == 0) {
         interactiveMode = true;
     }
 
-    char* strBuffer = (char*)calloc(STR_BUFF_SIZE, sizeof(char));
-    assert(strBuffer != NULL);
+    /** this is a buffer for reading instructions. */
+    char* instructionBuffer = (char*)calloc(STR_BUFF_SIZE, sizeof(char));
+    assert(instructionBuffer != NULL);
     
     /** Parse strings and run the process using fork(). */
     while (!feof(fp)) {
-        /** infinite loop */
         if (interactiveMode) {
             printf("prompt> ");
         }
 
-        if ( fgets(strBuffer, STR_BUFF_SIZE, fp) != NULL ) {
+        if ( fgets(instructionBuffer, STR_BUFF_SIZE, fp) != NULL ) {
             /** fgets catches '\n' */
-            strtok(strBuffer, "\n"); // remove '\n'. If you cannot understand, learn how strtok() is implemented.
-            /** Parse user inputs and run the process using fork(). */
 
+            /** remove '\n'. If you cannot understand below, learn how strtok() works. */
+            strtok(instructionBuffer, "\n");
+
+            /** print instruction when batch mode. */
             if (interactiveMode == false) {
-                // batch mode
-                printf("%s\n", strBuffer);
+                /** batch mode */
+                printf("%s\n", instructionBuffer);
             }
 
+            /** Parse user inputs and run the process using fork(). */
             int numberOfChildProcesses = 0;
-            parseAndExecute(strBuffer, &numberOfChildProcesses);
+            parseAndExecute(instructionBuffer, &numberOfChildProcesses);
 
-            // run child processes simultaneously
+            /** run child processes simultaneously */
             for (int i = 0; i < numberOfChildProcesses; ++i) {
                 wait(NULL);
             }
 
-            memset(strBuffer, 0, STR_BUFF_SIZE);
+            memset(instructionBuffer, 0, STR_BUFF_SIZE);
         } else {
             /* end of the file */
             if (interactiveMode) {
@@ -106,66 +103,69 @@ void readInstruction(const char* filePath){
                 }
                 exit(EXIT_FAILURE);
             } else {
-                // do nothing.
-                // next while loop, while loop ends.
+                /** do nothing.
+                  * next while loop, loop ends. */
             }
         }
     }
 
-    free(strBuffer);
+    free(instructionBuffer);
 }
 
 void parseAndExecute(char* inputString, int* numberOfChildProcesses) {
-    // statement is partitioned by ';'.
-    // statement is composed of programPath and options.
+    /** Parse user inputs and run the process using fork(). */
 
-    // statement tokenzation
+    /** statement is partitioned by ';'.
+      * statement is composed of programPath and options. */
 
-
+    /** statement tokenization start */
     int numberOfStatements = 1;
+    int statementArgsIdx = 0;
     char** statementArgs = (char**)calloc(numberOfStatements, sizeof(char*));
     assert(statementArgs != NULL);
 
-    // options Tokenization
     char* statementArgsToken;
     const char delimitorOfStatement[2] = ";";
-    int statementArgsIdx = 0;
 
-    // get the first token
+    /** get the first token */
     statementArgsToken = strtok(inputString, delimitorOfStatement);
     if (statementArgsToken == NULL) {
-        // no program path.
+        /** no program path. */
         return;
     }
-    statementArgs[statementArgsIdx++] = statementArgsToken; // first statement
+
+    /** first statement */
+    statementArgs[statementArgsIdx++] = statementArgsToken; 
 
     while (true) {
         statementArgsToken = strtok(NULL, delimitorOfStatement);
+
+        /** no token */
         if (statementArgsToken == NULL) {
-            // end of the loop.
+            /** end of the loop. */
             break;
         }
 
-        // if the allocated memory is not enough, reallocate.
+        /** if the allocated memory is not enough, reallocate. */
         if (numberOfStatements == statementArgsIdx) {
             numberOfStatements *= 2;
             statementArgs = (char**)realloc(statementArgs, numberOfStatements * sizeof(char*));
             assert(statementArgs != NULL);
         }
 
+        /** add a token to tokenArray */
         statementArgs[statementArgsIdx++] = statementArgsToken;
     }
-    statementArgsIdx--; // 
 
     if (DEBUGGING) {
-        for (int i = 0; i <= statementArgsIdx; ++i) {
+        for (int i = 0; i < statementArgsIdx; ++i) {
             printf("statement %d: %s\n", i, statementArgs[i]);
         }
     }
 
-    // for every statment
-    for (int i = 0; i <= statementArgsIdx; ++i) {
-        //remove space before the programPath
+    /** for every statment */
+    for (int i = 0; i < statementArgsIdx; ++i) {
+        /** remove spacebar before the programPath */
         while ( *(statementArgs[i]) == ' ') {
             statementArgs[i]++;
         }
@@ -174,32 +174,45 @@ void parseAndExecute(char* inputString, int* numberOfChildProcesses) {
             printf("statement: %s\n", statementArgs[i]);
         }
 
-        // optionArgs intialization
-        int numberOfOptionArgs = 2;
+        /** optionArgs intialization */
+        int numberOfOptionArgs = 1;
+        int optionArgsIdx = 0;
         char** optionArgs = (char**)calloc(numberOfOptionArgs, sizeof(char*));
         assert(optionArgs != NULL);
 
-        // options Tokenization
+        /** options Tokenization start */
         char* optionArgsToken;
         const char delimitorOfOptions[2] = " ";
-        int optionArgsIdx = 0;
 
-        // get the first token
+        /** get the first token */
         optionArgsToken = strtok(statementArgs[i], delimitorOfOptions);
-        optionArgs[optionArgsIdx++] = optionArgsToken; //optionArgs[0] is program path.
+        if (optionArgsToken == NULL) {
+            /** no program path. */
+            return;
+        }
 
-        while (optionArgsToken != NULL) {
-            // if the allocated memory is not enough, reallocate.
+        /** optionArgs[0] is program path. */
+        optionArgs[optionArgsIdx++] = optionArgsToken; 
+
+        while (true) {
+            optionArgsToken = strtok(NULL, delimitorOfOptions);
+
+            /** no token */
+            if (optionArgsToken == NULL) {
+                /** end of the loop. */
+                break;
+            }
+
+            /** if the allocated memory is not enough, reallocate. */
             if (numberOfOptionArgs == optionArgsIdx) {
                 numberOfOptionArgs *= 2;
                 optionArgs = (char**)realloc(optionArgs, numberOfOptionArgs * sizeof(char*));
                 assert(optionArgs != NULL);
             }
 
-            optionArgsToken = strtok(NULL, delimitorOfOptions);
+            /** add a token to tokenArray */
             optionArgs[optionArgsIdx++] = optionArgsToken;
         }
-        optionArgsIdx--; // 
 
         if (DEBUGGING) {
             printf("Token: %s", optionArgs[0]);
@@ -210,16 +223,16 @@ void parseAndExecute(char* inputString, int* numberOfChildProcesses) {
         }
 
         if (optionArgs[0] == NULL) {
-            // no program path. do nothing.
+            /** no program path. do nothing. */
             continue;
         }
 
-        // if "quit" is inputted, exit the program.
+        /** if "quit" is inputted, exit the program. */
         if (strncmp(optionArgs[0], "quit", sizeof("quit")) == 0) {
             exit(0);
         }
 
-        // fork and execute
+        /** fork and execute */
         if (fork() == 0) {
             /** child proess */
             execvp(optionArgs[0], optionArgs);
